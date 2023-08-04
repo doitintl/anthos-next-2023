0eb7b90bd5f819d5d1bdd37400fce79d042ba09b
Dave Cavaletto <dave.c@doit-intl.com>
2023-08-04 14:33:40 -0600

    initial working docker image can take a picture

--- cv/main.py
+++ cv/main.py
@@ -4,10 +4,11 @@ import sys
 if __name__ == "__main__":
     print( "Hello World!")
     capture = cv2.VideoCapture(0)
+
     if not capture.isOpened():
+        print("no camera found")
         sys.exit(1)
 
     ret, frame = capture.read()
 
-    cv2.imwrite('{}_{}.{}'.format("/images/test.jpg"), frame)
-
+    cv2.imwrite("/images/test.jpg", frame)
