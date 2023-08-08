5725442b565ea524219605113bbf916e6caf33c1
Dave Cavaletto <dave.c@doit-intl.com>
2023-08-08 14:45:37 -0600

    setup capture app

---
+++ cv/README.md
@@ -0,0 +1,3 @@
+# Build
+
+    gcloud builds submit --project=davec-anthos-next --tag us-west1-docker.pkg.dev/davec-anthos-next/docker/capture:1.0  
--- cv/main.py
+++ cv/main.py
@@ -1,14 +1,56 @@
+from tempfile import TemporaryFile
 import cv2
-import sys
+from flask import Flask
+import datetime
+import os
+from minio import Minio
 
-if __name__ == "__main__":
-    print( "Hello World!")
+client = Minio(
+    'http://localhost:9000/',
+    access_key="D7GROicQiR4UGVl1CRVh",
+    secret_key="SAYxL5bcEXu6k0LrrOGypwRw6Aqmmi3GgFJFurSu",
+)
+
+
+def capture_image():
     capture = cv2.VideoCapture(0)
 
     if not capture.isOpened():
         print("no camera found")
-        sys.exit(1)
+        return False
 
     ret, frame = capture.read()
 
-    cv2.imwrite("/images/test.jpg", frame)
+    filename = f".{datetime.datetime.utcnow().isoformat()}.jpg"
+
+
+    with TemporaryFile() as temp:
+        print(f"capturing image to tempfile {temp.name}")
+        cv2.imwrite(temp.name, frame)
+        capture.release()
+        print(f"putting image to bucket {filename}")
+        client.fput_object(
+            "images", filename, temp.name,
+        )
+    return True
+
+
+# write a small flask REST api to capture image
+
+app = Flask(__name__)
+
+
+@app.route("/capture")
+def capture():
+    stat = capture_image()
+    return "captured" if stat else "failed"
+
+
+@app.route("/notify")
+def notify():
+    print("image create notified")
+    return "OK"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
