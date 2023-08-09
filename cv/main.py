10b524de8376ad641dc092e44d95861992085d14
Dave Cavaletto <dave.c@doit-intl.com>
2023-08-09 15:16:45 -0600

    don't copy to minio

--- cv/main.py
+++ cv/main.py
@@ -5,6 +5,24 @@ import datetime
 import os
 from minio import Minio
 
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
 app = Flask(__name__)
 
 client = Minio(
@@ -25,9 +43,9 @@ def capture_image():
             output = os.system(f"v4l2-ctl --device /dev/video0 --set-fmt-video=width=640,height=480,pixelformat=MJPG --stream-mmap --stream-to={temp.name} --stream-count=1")
             app.logger.info(f"v4l2-ctl output: {output}")
             app.logger.info(f"putting image to bucket {filename}")
-            client.fput_object(
-                "images", filename, temp.name,
-            )
+            # client.fput_object(
+            #     "images", filename, temp.name,
+            # )
 
         return True
     except Exception as e:
