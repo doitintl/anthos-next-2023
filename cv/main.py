b3b97e48da07029193a47c39b8f287fb176138b2
Dave Cavaletto <dave.c@doit-intl.com>
2023-08-22 15:49:24 -0600

    improve logging

--- cv/main.py
+++ cv/main.py
@@ -2,6 +2,7 @@ import io
 import json
 from tempfile import NamedTemporaryFile
 from prometheus_flask_exporter import PrometheusMetrics
+from middleware import logger
 
 from flask import Flask, request, render_template, Response
 import datetime
@@ -14,30 +15,6 @@ from google.cloud import pubsub_v1
 # OCR libraries
 from PIL import Image
 import pytesseract
-
-
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {
-        'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    },
-    'console': {
-        "class": "logging.StreamHandler",
-        "stream": "ext://sys.stdout",
-        "formatter": "default",
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['console']
-    }
-})
-
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
@@ -57,26 +34,26 @@ if os.environ["IS_CAPTURING"] == "true":
         try:
 
             filename = f"{datetime.datetime.utcnow().isoformat()}.jpg"
-            app.logger.info(f"filename is {filename}")
+            logger.info('filename', filename=filename)
             with NamedTemporaryFile(suffix=".jpg") as temp:
 
-                app.logger.info(f"capturing image to tempfile {temp.name}")
+                logger.info("capturing image to tempfile", tempfile=temp.name)
                 output = os.system(f"v4l2-ctl --device /dev/video0 --silent --set-fmt-video=width=640,height=480,pixelformat=MJPG --stream-mmap --stream-to={temp.name} --stream-count=1")
-                app.logger.info(f"v4l2-ctl output: {output}")
+                logger.info("v4l2-ctl output", output=output)
                 if int(output) != 0:
-                    app.logger.info(f"v4l2-ctl failed with exit code {output}")
+                    logger.error("v4l2-ctl failed with exit code", output=output)
                     return {"message": "error", "error": output}, 500
-                app.logger.info(f"putting image to bucket {filename}")
+                logger.info("putting image to bucket", filename=filename)
                 minio_client.fput_object("images", filename, temp.name)
 
-                app.logger.info(f"writing image to static/image.jpg")
+                logger.info("writing image to static/image.jpg")
                 # always overwrite image.jpg with the latest image
                 with open("static/image.jpg", "wb") as f:
                     f.write(temp.read())
 
             return {"message": "OK", "image_name": filename}, 200
         except Exception as e:
-            app.logger.info(f"failed to capture image: {e}")
+            logger.error("failed to capture image", error=e)
             return {"message": "error", "error": e}, 500
         
 
@@ -95,9 +72,9 @@ if os.environ["IS_CAPTURING"] == "true":
     def messages(filename):
 
         temp = NamedTemporaryFile(suffix=".txt")
-        app.logger.info(f"downloading messages {filename}.txt to tempfile {temp.name}")
+        logger.info("downloading message file to tempfile", tempfile=temp.name, message_file=f"${filename}.txt")
         minio_client.fget_object("cv-output", f"{filename}.txt", temp.name)
-        app.logger.info(f"text downloaded to tempfile {temp.name}")
+        logger.info("text downloaded to tempfile", tempfile=temp.name)
 
         with open(temp.name) as read_file:
             rtn = {"message": "OK", "messages": read_file.read()}, 200
@@ -110,18 +87,18 @@ else:
 
     @app.route("/notify", methods=['POST'])
     def notify():
-        app.logger.info("image create notified")
-        app.logger.info(f"request json {request.json}")
+        logger.info("image create notified")
+        logger.info("request json", request_json=request.json)
 
         image_name = request.json["Key"]
 
         with NamedTemporaryFile(suffix=".jpg") as temp:
             (bucket, filename) = image_name.split("/")
-            app.logger.info(f"downloading image {bucket}/{filename} to tempfile {temp.name}")
+            logger.info("downloading image to tempfile", image=f"{bucket}/{filename}", tempname=temp.name)
             minio_client.fget_object(bucket, filename, temp.name)
-            app.logger.info(f"image downloaded to tempfile {temp.name}")
+            logger.info("image downloaded to tempfile", tempfile=temp.name)
             msg = pytesseract.image_to_string(Image.open(temp.name))
-            app.logger.info(f"image text: {msg}")
+            logger.info("image text", image_text=msg)
         
         msg = "no text found" if msg == "" else msg
         
